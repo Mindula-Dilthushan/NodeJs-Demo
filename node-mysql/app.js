f7c0f8dc33a63c1e7eda9931a8c634c8b0dc5fb0
Mindula Dilthushan <minduladilthushan1@gmail.com>
2021-01-29 16:45:18 +0530

    handlebars

--- node-mysql/app.js
+++ node-mysql/app.js
@@ -1,17 +1,28 @@
 //NodeJs Project
 //Mindula-Dilthushan
 const express = require("express");
+const path = require('path');
 const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({path: './.env'})
+
 const app = express();
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1023',
-    database: 'nodejs-project'
+const connection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    insecureAuth : true
 });
 
-db.connect( (error) => {
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+console.log(__dirname);
+app.set('view engine','hbs');
+
+connection.connect( (error) => {
     if (error) {
         console.log(error)
     } else {
@@ -20,9 +31,10 @@ db.connect( (error) => {
 })
 
 app.get("/", (req, res) => {
-        res.send("<H1>Node-MySQL</H1>")
+        // res.send("<H1>Node-MySQL</H1>")
+    res.render("index.html");
 });
 
-app.listen(5000, () => {
-    console.log("Server Started On Port 5000");
+app.listen(6000, () => {
+    console.log("Server Started On Port 6000");
 })
